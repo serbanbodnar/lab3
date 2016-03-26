bda206d7456f80ddc7d6d78f45e3525bacfc1a57
alin dradici <alindradici@gmail.com>
2016-03-26 13:06:45 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("salut lume");
+        System.out.println("salut lume 2");
+        System.out.println("salut lume 3");
+    }
+}
